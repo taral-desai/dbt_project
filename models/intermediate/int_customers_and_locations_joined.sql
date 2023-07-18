f28cf49b1d138701e959ff25a59229e22d4da391
kds-taral <77844108+taral-desai@users.noreply.github.com>
2023-07-18 20:31:54 +0000

    update intermediate model

--- models/intermediate/int_customers_and_locations_joined.sql
+++ models/intermediate/int_customers_and_locations_joined.sql
@@ -6,26 +6,43 @@ customers as (
 
 ),
 
-orders as (
+cities as (
 
-    select * from {{ ref('orders') }}
+    select * from {{ ref('stg_tech_store__cities') }}
 
 ),
 
-total_revenue_and_units_by_customer as (
+states as (
 
-    select 
+    select * from {{ ref('stg_tech_store__states') }}
+
+),
+
+zip_codes as (
+
+    select * from {{ ref('stg_tech_store__zip_codes') }}
+
+),
+
+customers_and_locations_joined as (
+
+    select
         customers.customer_id,
-        sum(orders.amount_in_usd) as total_revenue_in_usd,
-        sum(orders.quantity) as total_quantity
-    
-    from orders
+        cities.city_name,
+        states.state_name,
+        zip_codes.zip_code
 
-    left join customers
-        on orders.customer_id = customers.customer_id
+    from customers
 
-    group by 1
+    left join cities
+        on customers.city_id = cities.city_id
+
+    left join states
+        on cities.state_id = states.state_id
+
+    left join zip_codes
+        on cities.zip_code_id = zip_codes.zip_code_id
 
 )
 
-select * from total_revenue_and_units_by_customer
+select * from customers_and_locations_joined
